795157615351262bf23aca91f0df8e1854c96761
Deven Phillips <dphillips@juggl.us>
2015-10-09 01:09:19 -0400

    Converted to basic HttpServlet

--- src/main/java/us/juggl/hello/Hello.java
+++ src/main/java/us/juggl/hello/Hello.java
@@ -1,7 +1,20 @@
 package us.juggl.hello;
 
-class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World.");
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+class Hello extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        resp.setStatus(200);
+        out.print("Hello World");
+        out.flush();
+        out.close();
     }
 }
